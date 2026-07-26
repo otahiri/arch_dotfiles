7160dff2370bd6103060aedbb0a5d65f0b4aefb6
otahiri <chucy0066@gmail.com>
2026-07-26 16:14:55 +0100

    feat: added smear cursor plugin and fixed the line lenght for black formatter

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -735,6 +735,11 @@ require("lazy").setup({
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				-- javascript = { "prettierd", "prettier", stop_after_first = true },
 			},
+			formatters = {
+				black = {
+					prepend_args = {'--line-length', '79'},
+				},
+			},
 		},
 	},
 
