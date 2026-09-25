9ed2277a951fcdb7dffc840d6a84b4f30e20a18c
otahiri <chucy0066@gmail.com>
2026-09-25 11:24:13 +0000

    feat: added smear cursor back(it looks soo good man  i cannot give it up)

---
+++ nvim/.config/nvim/plugin/smear_cursor.lua
@@ -0,0 +1,11 @@
+vim.pack.add({
+	{src = "https://github.com/sphamba/smear-cursor.nvim",}
+})
+
+require('smear_cursor').setup({
+	smear_between_buffers = true,
+    	smear_between_neighbor_lines = true,
+    	scroll_buffer_space = true,
+    	legacy_computing_symbols_support = false,
+    	smear_insert_mode = true,
+})
